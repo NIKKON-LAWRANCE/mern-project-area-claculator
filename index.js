85b5c877be989db53a9e0e022998a1f0c5b50710
nikkon-roy <nikkonlawrance@gmail.com>
2022-08-05 17:26:13 +0600

    Final commit

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const experss = require('express');
+const bodyParser = require('body-parser');
 const app = experss();
 const PORT = 3001;
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.get("/",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
@@ -10,6 +17,19 @@ app.get("/circle",(req,res)=>{
 });
 app.get("/triangle",(req,res)=>{
     res.sendFile(__dirname+"/taiangle.html");
+});
+
+app.post("/triangle",(req,res)=>{
+    const height = req.body.height;
+    const base = req.body.base;
+    const area = 0.5*base*height;
+    res.send(`<h2>Area of Triangle is:${area}</h2>`);
+});
+
+app.post("/circle",(req,res)=>{
+    const radius = req.body.radius;
+    const area = Math.PI * radius * radius;
+    res.send(`<h2>Area of Circle is:${area}</h2>`);
 });
 
 app.listen(PORT,()=>{
